c96187e7e99d6e4235831de141c8af047fe9dfab
Tomasz <Tomasz@DESKTOP-PIUN9TU>
2026-02-08 15:01:21 +0100

    Add project files.

---
+++ FitLog/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using FitLog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
